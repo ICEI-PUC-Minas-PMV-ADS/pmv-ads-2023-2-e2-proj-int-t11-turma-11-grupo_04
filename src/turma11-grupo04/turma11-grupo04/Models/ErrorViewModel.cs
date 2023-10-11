10b451f792821c2fe1ecd5b7903de1448f17144d
Lucas Bezerra Sampaio <lbsampaio@sga.pucminas.br>
2023-10-10 21:57:22 -0300

    Correção: Removendo Source do repositório

--- src/turma11-grupo04/turma11-grupo04/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace turma11_grupo04.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
