fc4d5ee00c411f44338a92f1e2c5326935cd1ed4
GleystonGSilva <124820165+GleystonGSilva@users.noreply.github.com>
2023-10-26 14:58:36 -0300

    Pagina de login

---
+++ src/Turma11-grupo04/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using turma11_grupo04.Models;
+using Turma11_grupo04.Models;
+
+
+namespace Turma11_grupo04.Controllers
+{
+    public class LoginController : Controller
+    {
+
+        private readonly AppDbContext _context;
+        public LoginController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Entrar(LoginModel LoginModel)
+        {
+
+
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+
+                    var cobrador = _context.Cobrador.FirstOrDefault
+                        (c => c.Email == LoginModel.Email && c.Senha == LoginModel.Senha);
+
+
+
+                    if (cobrador != null)
+                    {
+                        return RedirectToAction("Index", "Home");
+
+                    }
+                    else
+                    {
+
+                        ModelState.AddModelError("", "Credenciais inválidas. Verifique seu email e senha.");
+                    }
+
+                }
+
+                return View("Index");
+            }
+
+            catch (Exception)
+            {
+
+                TempData["MensagemErro"] = $"Ops, não conseguimos realizar seu login, tente novamente.";
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}   
---
+++ src/Turma11-grupo04/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Turma11_grupo04.Models
+{
+    public class LoginModel
+    {
+
+
+        [Required(ErrorMessage = "É obrigatório informar o E-mail.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "É obrigatório informar a senha.")]
+        public string Senha { get; set; }
+
+    }
+}
