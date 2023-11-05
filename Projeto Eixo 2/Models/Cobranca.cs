572ba9be1043b6a56dcff010d82f373269f88daa
Cassio Kawamura <cassio.kawamura@sga.pucminas.br>
2023-11-06 05:12:17 +0900

    Alterações

---
+++ Projeto Eixo 2/Models/Cobranca.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Eixo_2.Models
+{
+    [Table("Cobranças")]
+    public class Cobranca
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ClienteId { get; set; }
+
+        [ForeignKey("ClienteId")]
+        [Required(ErrorMessage = "Obrigatório informar o cliente.")]
+        public Cliente Cliente { get; set; }
+    }
+}
