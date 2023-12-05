b770ab8cd256a9a52b4a7fb4372ce751fb5c52b5
Lucas Bezerra Sampaio <lbsampaio@sga.pucminas.br>
2023-12-04 22:43:06 -0300

    Refatorando código da tela Detail, corrigindo envio de email e adicionando persistência na hora de carregar a tabela da página.

--- src/Projeto Eixo 2/Controllers/CobradoresController.cs
+++ src/Projeto Eixo 2/Controllers/CobradoresController.cs
@@ -1,17 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
-using Projeto_Eixo_2.Models;
-using Microsoft.AspNetCore.Hosting;
-using System.IO;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
+
 
 namespace Projeto_Eixo_2.Controllers
 {
